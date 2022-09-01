934522199d72c1bbae213cf2d91d9b8a22233249
NicoEberlein <nico@eberln.de>
2022-09-01 12:07:53 +0200

    created CountdownDataRepository to be able to have different datasources in the future

---
+++ src/main/java/de/eberln/schleifenPi/datahandling/CountdownDataRepositoryFilesystemImpl.java
@@ -0,0 +1,52 @@
+package de.eberln.schleifenPi.datahandling;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Repository
+public class CountdownDataRepositoryFilesystemImpl implements CountdownDataRepository{
+
+	@Value("${application.pathToCountdownData}")
+	private String pathToCountdownData;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	public ResponseEntity<Object> saveCountdownData(CountdownData countdownData) {
+		
+		try {
+			mapper.writeValue(new File(pathToCountdownData), countdownData);
+		}catch(IOException e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+		
+	}
+	
+	public ResponseEntity<CountdownData> readCountdownData() {
+		
+		try {
+			
+			CountdownData countdownData = mapper.readValue(new File(pathToCountdownData), CountdownData.class);
+			return new ResponseEntity<CountdownData>(countdownData, HttpStatus.OK);
+			
+		}catch(IOException e) {
+			
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		
+		}
+		
+	}
+	
+}
