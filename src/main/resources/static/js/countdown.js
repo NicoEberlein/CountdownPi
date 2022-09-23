4e1dd6bce0da3a7b4b1d6af8ca9f82237ced3809
NicoEberlein <nico@eberln.de>
2022-09-23 21:33:26 +0200

    implemented countdown feature

--- src/main/resources/static/js/countdown.js
+++ src/main/resources/static/js/countdown.js
@@ -8,9 +8,16 @@
     "date": "2022-05-10T09:18:00.000Z"
 }*/
 
+var eventDate;
+
 window.onload = function() {
     receiveCountdownData();
     window.setInterval(receiveCountdownData, 5000);
+    window.setInterval(function() {
+        var timeDiff = calculateTimeDiff(eventDate);
+        document.getElementById("ot-countdown-hours").innerHTML = timeDiff.hours;
+        document.getElementById("ot-countdown-minutes").innerHTML = timeDiff.minutes;
+    }, 1000);
 }
 
 function receiveCountdownData() {
@@ -18,18 +25,16 @@ function receiveCountdownData() {
 }
 
 function processCountdownData(data) {
-    data.date = new Date(data.date);
 
     if(data.operationType === "COUNTDOWN") {
-
         document.getElementById("ot-countdown-countdown").hidden = false;
         document.getElementById("ot-message-text").hidden = true;
 
-    }else if(data.operationType === "MESSAGE") {
+        eventDate = data.date;
 
+    }else if(data.operationType === "MESSAGE") {
         document.getElementById("ot-message-text").hidden = false;
         document.getElementById("ot-countdown-countdown").hidden = true;
-
     }
 
     if(data.backgroundMode === "BLURREDIMAGE") {
@@ -39,10 +44,39 @@ function processCountdownData(data) {
         document.body.style.background = data.color;
     }
 
-    document.getElementById("ot-both-image").setAttribute("src", window.location.origin + "/rest/getImage/" + data.image);
+    document.getElementById("ot-both-logo").setAttribute("src", window.location.origin + "/rest/getImage/" + data.image);
     document.getElementById("ot-both-heading").innerHTML = data.heading;
 
 }
+
+function calculateTimeDiff(dateInFuture) {
+
+    var currentDate = new Date();
+    var dateInFuture = new Date(dateInFuture);
+
+    var milliDiff = dateInFuture.getTime() - currentDate.getTime();
+
+    var timeDiff={
+        hours: 0,
+        minutes: 0
+    };
+
+    if(milliDiff > 0) {
+
+        var minutes = Math.floor(milliDiff / 1000 / 60);
+        if(minutes > 60) {
+            timeDiff.hours = Math.floor(minutes / 60);
+            timeDiff.minutes = minutes - timeDiff.hours * 60;
+        }
+
+    }else{
+        throw "The specified date is in the past.";
+    }
+
+    return timeDiff;
+
+}
+
 
 function sendRequest(functionToCall, url, type, body, headers){
     let request = {
